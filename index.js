3790b85a61fb7d03e25339b869d3c752aec1c2b6
yazen firas <psyazen3@gmail.com>
2020-06-03 10:06:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const [moo, neigh, baa, oink, cluck] = 'cow horse sheep pig chicken'.split(" ");
 const [Bessie, , Dolly, Babe, Little] = 'cow horse sheep pig chicken'.split(" ")
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of black_and_white, black, and pink.
-
+const [black_and_white, , black, pink] = 'cow horse sheep pig chicken'.split(" ")
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
